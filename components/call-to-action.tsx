c237b2a85dc2fb19ab7d7106b8c7185ff61189eb
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-11 14:36:02 +0530

    acll to action animation

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -1,24 +1,54 @@
+"use client";
+
 
 import helixImage from "@/assets/images/helix2.png";
 import emojiStar from "@/assets/images/emojistar.png";
 import Image from "next/image";
 
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
 
 
 export const CallToAction = () => {
+    
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ["start end", "end end"]
+    });
+
+    const translateY = useTransform(scrollYProgress, [0, 1], [40, -40]);
+
     return (
-        <div className="bg-black text-white text-center py-[72px] sm:py-24">
+        <div 
+            ref={containerRef} 
+            className="bg-black text-white text-center py-[72px] sm:py-24"
+        >
             <div className="container max-w-xl relative">
-                <Image 
-                    src={helixImage}
-                    alt="Helix logo"
-                    className="absolute -top-[164px] left-[calc(100%-140px)]"
-                />
-                <Image 
-                    src={emojiStar}
-                    alt="styar logo"
-                    className="absolute -top-[120px] right-[calc(100%+24px)]"
-                />
+                <motion.div
+                    style={{
+                        translateY: translateY
+                    }}
+                >
+                    <Image 
+                        src={helixImage}
+                        alt="Helix logo"
+                        className="absolute top-6 left-[calc(100%-4px)] hidden xl:block"
+                    />
+                </motion.div>
+                <motion.div
+                    style={{
+                        translateY: translateY
+                    }}
+                >
+                    <Image 
+                        src={emojiStar}
+                        alt="styar logo"
+                        className="absolute -top-[120px] right-[calc(100%+24px)] hidden: md:block"
+                    />
+                </motion.div>
                 <h2 className="font-bold text-5xl sm:text-6xl tracking-tighter">Get instant access</h2>
                 <p className="text-xl mt-5 text-white/70">
                     Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
