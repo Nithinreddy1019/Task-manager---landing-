0f6f780c1e37c89db597c1423d00e452a028d4de
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-11 14:44:04 +0530

    deployment fix

--- components/logo-ticker.tsx
+++ components/logo-ticker.tsx
@@ -25,6 +25,7 @@ export const LogoTicker = () => {
     return (
         <div className="bg-black text-white py-[72px] sm:py-24">
             <div className="container">
+                  {/* eslint-disable-next-line react/no-unescaped-entities */}
                 <h2 className="text-xl text-center text-white/70">Trusted by the world's most innovative teams</h2>
                 <div className="overflow-hidden mt-9 relative  before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5   before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] before:z-10">
                     <motion.div
